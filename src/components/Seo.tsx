1fe6c14fe834028e8ae64b8e309caf2e16eecd1d
rishikamehta2004@gmail.com <rishikamehta2004@gmail.com>
2026-07-18 12:39:02 +0800

    improve SEO

--- src/components/Seo.tsx
+++ src/components/Seo.tsx
@@ -1,7 +1,7 @@
 import { useEffect } from "react";
 import { useLocation } from "react-router-dom";
 
-const SITE_URL = "https://ntuwit-website.vercel.app";
+const SITE_URL = "https://www.ntuwit.com";
 
 type SeoDetails = {
   title: string;
